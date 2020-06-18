caa54640b17f24d5cfd839aeaddffa125108a33a
Kotone Itaya <kotone@sfc.keio.ac.jp>
2020-06-18 13:13:38 +0900

    gts-bio 0.9.4

--- Formula/gts-bio.rb
+++ Formula/gts-bio.rb
@@ -1,8 +1,14 @@
 class GtsBio < Formula
   desc "GTS: The Genomics Tool suite"
   homepage "https://github.com/go-gts/gts"
-  url "https://github.com/go-gts/gts/archive/v0.9.3.tar.gz"
-  sha256 "718f45b520f0fadc39fc43d88ba183bbe50cf77f7f44852aba6147005c48a4a4"
+  url "https://github.com/go-gts/gts/archive/v0.9.4.tar.gz"
+  sha256 "16df8207b9708cf4f11dc150c930843c7676ce9a6d0f428050107371db77f8cc"
+
+  bottle do
+    root_url "https://dl.bintray.com/go-gts/bottles-gts"
+    cellar :any_skip_relocation
+    sha256 "2f841ce4add6c057b8827a38244c5b7faec418e2690f6cbac187f2d105fa0299" => :catalina
+  end
 
   depends_on "go" => :build
 
