1c048da18dc51da7235b6a5cba0a22c415730b0b
goreleaserbot <goreleaser@carlosbecker.com>
2020-07-01 01:44:14 +0900

    Brew formula update for gts version v0.11.4

--- Formula/gts-bio.rb
+++ Formula/gts-bio.rb
@@ -2,16 +2,16 @@
 class GtsBio < Formula
   desc "GTS: the Genomics Tools Suite"
   homepage "https://github.com/go-gts/gts"
-  version "0.11.3"
+  version "0.11.4"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/go-gts/gts/releases/download/v0.11.3/gts_0.11.3_Darwin_x86_64.tar.gz"
-    sha256 "eedbe63113ccf800e3436c28fd058cb29539f25384e1d59b2bb336447456ab7c"
+    url "https://github.com/go-gts/gts/releases/download/v0.11.4/gts_0.11.4_Darwin_x86_64.tar.gz"
+    sha256 "a2823141b56d1b30fd4b39f05ca6e2a62edae9ee39f0fd043ee85543249af5da"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/go-gts/gts/releases/download/v0.11.3/gts_0.11.3_Linux_x86_64.tar.gz"
-      sha256 "aadc8c4301d29d5ef77e1e0b5647ac67e41ee9bc3f4786b3dcd739725c317a69"
+      url "https://github.com/go-gts/gts/releases/download/v0.11.4/gts_0.11.4_Linux_x86_64.tar.gz"
+      sha256 "96c92d2d6b12ea7ae2bcb4de46221c1559704c2d0cd3d8b91c1f7cb5416d7ec6"
     end
   end
   
